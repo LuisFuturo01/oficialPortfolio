892919672378bcf551eecc82aea4dfb40b0788f3
Luis Zeballos <luis.futuro.01@gmail.com>
2025-12-01 00:43:06 -0400

    blank link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => (
                 <p className="rights">Todos los derechos reservados.</p>
             </div>
             <div className="footer-right">
-                <a href="https://github.com/LuisFuturo01" className="footer-social-link"><Github size={20} /></a>
-                <a href="https://www.linkedin.com/in/luis-alejandro-zeballos-quiroz-324bab2b9/" className="footer-social-link"><Linkedin size={20} /></a>
-                <a href="mailto:luis.futuro.01@gmail.com" className="footer-social-link"><Mail size={20} /></a>
+                <a target="_blank" href="https://github.com/LuisFuturo01" className="footer-social-link"><Github size={20} /></a>
+                <a target="_blank" href="https://www.linkedin.com/in/luis-alejandro-zeballos-quiroz-324bab2b9/" className="footer-social-link"><Linkedin size={20} /></a>
+                <a target="_blank" href="mailto:luis.futuro.01@gmail.com" className="footer-social-link"><Mail size={20} /></a>
             </div>
         </div>
         <div className="footer-bottom-line"></div>
