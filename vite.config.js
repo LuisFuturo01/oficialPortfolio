1e89fc736b40ffbb07e62c3c622e7788af92b13c
Luis Zeballos <luis.futuro.01@gmail.com>
2025-12-03 11:35:57 -0400

    aplicando pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import VitePWA from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Portafolio Luis Zeballos',
+        short_name: 'Luis Zeballos',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#00381cff',
+        icons: [
+          {
+            src: '/src/assets/image/profile.jpg',
+            sizes: '192x192',
+            type: 'image/jpeg',
+          },
+        ],
+      },
+    }),
+  ],
+});
